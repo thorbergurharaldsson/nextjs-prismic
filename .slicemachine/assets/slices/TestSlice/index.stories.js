4d4e054d119993ec336cdb97dfce413d383d0aad
thorbergurharaldsson <thorbergur@haraldssonprod.is>
2022-03-07 23:37:46 +0000

    init nextjs app

---
+++ .slicemachine/assets/slices/TestSlice/index.stories.js
@@ -0,0 +1,9 @@
+import MyComponent from '../../../../slices/TestSlice';
+
+export default {
+  title: 'slices/TestSlice'
+}
+
+
+export const _DefaultSlice = () => <MyComponent slice={{"variation":"default-slice","name":"Default slice","slice_type":"test_slice","items":[],"primary":{"title":[{"type":"heading1","text":"E-enable plug-and-play mindshare","spans":[]}],"description":[{"type":"paragraph","text":"Ad ad anim laboris velit deserunt exercitation magna elit aute occaecat nostrud. Ad aliquip qui aliqua voluptate Lorem. Velit culpa ad est laborum eiusmod dolor dolore pariatur nisi.","spans":[]}]},"id":"_DefaultSlice"}} />
+_DefaultSlice.storyName = 'Default slice'
